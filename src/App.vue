ee1fc8ee873b10c920c5cdb7bcde2c5b58750b59
Cassandre <cassandre.yontailleux@gmail.com>
2025-02-25 15:03:18 +0100

    fin du 2e cours : probleme invertedPosts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
-//let text = "Ca va bien";
-const text = ref("hello");
+const text = ref("");
+const trimmedText = computed(()=> text.value.trim());// à chaque fois que "text" est modifiée, la fonction fléchée ()=>text.value.trim() va être utilisée
 
 // function updateText(event) {
 //   text.value = event.target.value;
 // }
 
 const posts = ref([]);
+const invertedPosts = computed(()=> posts.slice().sort((post2, post1) => post1.createdAt - post2.createdAt))
 
 function addPost() {
-  posts.value.push(text.value);
+  const newPost = {
+    id: Math.random().toString(36).substring(2), //UUID
+    content: trimmedText.value,
+    createdAt: new Date(),
+    author: {
+      username: "anonymous",
+      avatarURL: "https://avatars.githubusercontent.com/u/130103824?v=4"
+    }
+  }
+  posts.value.push(newPost);
   text.value = "";
 }
 </script>
@@ -28,13 +38,22 @@ function addPost() {
           - v-model = binding bi-latéral (v-bind et v-on en même temps)
           - v-for = permet de faire des foreach et des for
         -->
-        <button type="submit">Publier</button>
+        <button type="submit" :disabled="!trimmedText">Poster</button>
       </form>
 
-      <!--<p>{{ text }}</p>-->
-      <!--RENDU DECLARATIF-->
 
-      <p v-for="(post, index) in posts" :key="index">{{ post }}</p>
+      <p v-show="!posts.length"> Il n'y a rien par ici...</p>
+
+      <article v-for="(post, index) in /*invertedPosts*/posts" :key="index" class="card">
+        <header>
+          <img :src="post.author.avatarURL" :alt="post.author.username" width="36" height="36"/>
+          <!--RENDU DECLARATIF-->
+          <a>{{ post.author.username }}</a>
+          <a>{{ post.createdAt }}</a>
+        </header>
+        <!--RENDU DECLARATIF-->
+        <p>{{ post.content }}</p>
+      </article>
     </div>
   </main>
 </template>
@@ -80,5 +99,28 @@ button {
   font-size: 1rem;
   height: 40px;
   padding: 0 1rem;
+}
+button:disabled{
+  opacity: 0.4;
+  cursor: not-allowed;
+}
+article {
+  padding: 1rem;
+  overflow: hidden;
+}
+
+article p{
+  white-space: pre-wrap;
+}
+
+article header{
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  margin-bottom: 1rem;
+}
+
+article img{
+  border-radius: 50%;
 }
 </style>
