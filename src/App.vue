dc4e615ad161579302d5c1acef5be9a05e1c6a79
Maelys <joret.maelys@gmail.com>
2025-04-02 10:26:25 +0200

    cours routes protégées

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
+<script setup>
+import AppNavbar from "@/components/AppNavbar.vue";
+import { computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const router = useRouter();
+const route = useRoute();
+
+function logout() {
+  localStorage.removeItem("user");
+  router.push("/register");
+}
+
+const showNavbar = computed(() => ["login", "register"].includes(route.path));
+</script>
+
 <template>
-  <RouterView />
+  <AppNavbar v-if="showNavbar" />
+
+  <main>
+    <div class="container">
+      <RouterView />
+    </div>
+  </main>
 </template>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ArrowRightStartOnRectangleIcon } from "@heroicons/vue/24/outline";
+import { useUserData } from "@/composables/useUserData";
+
+const { user } = useUserData();
+
+function logout() {
+  localStorage.removeItem("user");
+  location.reload();
+}
+</script>
+
+<template>
+  <header class="navbar">
+    <div class="navbar__user" v-if="user">
+      <img :src="user.avatarUrl" alt="user avatar" class="avatar" height="36" width="36" />
+      <b>{{ user.username }}</b>
+    </div>
+
+    <button class="btn-icon btn-icon--large" @click="logout">
+      <ArrowRightStartOnRectangleIcon />
+    </button>
+  </header>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem;
+  border-bottom: 1px solid var(--color-border);
+  position: sticky;
+  top: 0;
+  background-color: var(--color-bg-primary);
+  z-index: 1;
+}
+.navbar__user {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
+</style>
